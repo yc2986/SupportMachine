32ffe25d851cb499096d2891c3017c75da537c6f
yc2986 <yc2986@columbia.edu>
2017-03-16 14:59:08 -0700

    add simple test for registration api

--- RegiHandler/tests.py
+++ RegiHandler/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from RegiHandler.serializers import DolbyUserSerializer
+# rest framework
+from rest_framework.test import APIClient
+
+client = APIClient()
 
 # Create your tests here.
+class DolbyUserSerializerTest(TestCase):
+    def test_create_user_full_data(self):
+        data = {
+            # basic info
+            'username': 'yc2986',
+            'password': '123456',
+            'email': 'chengyuzhou1992@gmail.com',
+            # profile info
+            'company': 'dolby laboratories',
+            'registration_code': '123456',
+            'phone_number': '1234567890',
+        }
+        response = client.post('/register/', data, format = 'json')
+        # check successful registration
+        self.assertEqual(response.status_code, 200)
